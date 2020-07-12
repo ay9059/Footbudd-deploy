3f0bb916d5e162d722363db79060826911a68e24
abhishekyadav <ay9059@rit.edu>
2020-07-12 15:11:16 -0400

    Updates

---
+++ precache-manifest.da0ef0c1f08f9c870eeeee7b7f473130.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3808a5e4e6667f7b3d4ca0e257005a05",
+    "url": "/Footbudd-deploy/index.html"
+  },
+  {
+    "revision": "4477cfeff5e7656ad461",
+    "url": "/Footbudd-deploy/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "420ad4d915f4312dcb9d",
+    "url": "/Footbudd-deploy/static/css/main.34eba325.chunk.css"
+  },
+  {
+    "revision": "4477cfeff5e7656ad461",
+    "url": "/Footbudd-deploy/static/js/2.1931e71d.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Footbudd-deploy/static/js/2.1931e71d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "420ad4d915f4312dcb9d",
+    "url": "/Footbudd-deploy/static/js/main.12e27a73.chunk.js"
+  },
+  {
+    "revision": "000f9bc84822369f3833",
+    "url": "/Footbudd-deploy/static/js/runtime-main.88547f12.js"
+  },
+  {
+    "revision": "9f9d6819a9eb0a39208e0efe76968621",
+    "url": "/Footbudd-deploy/static/media/g.9f9d6819.png"
+  },
+  {
+    "revision": "764ad9fc0fc4a60a5dcf8fb5b669992a",
+    "url": "/Footbudd-deploy/static/media/m.764ad9fc.png"
+  }
+]);
